66373b44e25c69a6000a16a6a9e793acdafe5b8d
hyeongjun im <limbs713@naver.com>
2024-05-22 14:31:04 +0900

    :tada: initial commit

---
+++ src/main/java/capstone1/stocker/StockerApplication.java
@@ -0,0 +1,13 @@
+package capstone1.stocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockerApplication.class, args);
+    }
+
+}
