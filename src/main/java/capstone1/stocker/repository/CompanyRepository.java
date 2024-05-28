6ffd05a435a2733a17e09307d9a555b037e7f9b2
hyeongjun im <limbs713@naver.com>
2024-05-28 15:34:29 +0900

    :sparkles: findByTicker 메소드 추가

--- src/main/java/capstone1/stocker/repository/CompanyRepository.java
+++ src/main/java/capstone1/stocker/repository/CompanyRepository.java
@@ -2,6 +2,7 @@ package capstone1.stocker.repository;
 
 import capstone1.stocker.domain.Company;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
@@ -9,4 +10,6 @@ public interface CompanyRepository extends JpaRepository<Company, Long> {
     List<Company> findAll();
 
     boolean existsByTicker(String ticker);
+
+    Optional<Company> findByTicker(String ticker);
 }
