c438b356c65ce8c70e9837048403eba7dbc90c1e
HoYoungChun <ho723@naver.com>
2024-06-01 00:30:53 +0900

    feat: 주별 url 순회하며 json 생성하도록

--- crawl/crawler.py
+++ crawl/crawler.py
@@ -10,65 +10,77 @@ from bs4 import BeautifulSoup
 
 # 클러스터링된 뉴스의 url
 URL_0304_0310 = "https://search.naver.com/search.naver?where=news&sm=tab_tnw&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sort=0&photo=0&field=0&pd=3&ds=2024.03.04&de=2024.03.10&mynews=0&office_type=0&office_section_code=0&news_office_checked=&related=1&docid=0010014553331&nso=so:r,p:from20240304to20240310,a:all"
-URL_0311_0317 = None
-URL_0318_0324 = None
+URL_0311_0317 = "https://search.naver.com/search.naver?where=news&sm=tab_tnw&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sort=0&photo=0&field=0&pd=3&ds=2024.03.11&de=2024.03.17&mynews=0&office_type=0&office_section_code=0&news_office_checked=&related=1&docid=0090005273401&nso=so:r,p:from20240311to20240317,a:all"
+URL_0318_0324 = "https://search.naver.com/search.naver?where=news&sm=tab_tnw&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sort=0&photo=0&field=0&pd=3&ds=2024.03.18&de=2024.03.24&mynews=0&office_type=0&office_section_code=0&news_office_checked=&related=1&docid=0150004963698&nso=so:r,p:from20240318to20240324,a:all"
+URL_0325_0331 = "https://search.naver.com/search.naver?where=news&sm=tab_tnw&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sort=0&photo=0&field=0&pd=3&ds=2024.03.25&de=2024.03.31&mynews=0&office_type=0&office_section_code=0&news_office_checked=&related=1&docid=0180005702954&nso=so:r,p:from20240325to20240331,a:all"
+URL_0401_0407 = "https://search.naver.com/search.naver?where=news&sm=tab_tnw&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sort=0&photo=0&field=0&pd=3&ds=2024.04.01&de=2024.04.07&mynews=0&office_type=0&office_section_code=0&news_office_checked=&related=1&docid=0920002327159&nso=so:r,p:from20240401to20240407,a:all"
+URL_0408_0414 = "https://search.naver.com/search.naver?where=news&sm=tab_tnw&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sort=0&photo=0&field=0&pd=3&ds=2024.04.08&de=2024.04.14&mynews=0&office_type=0&office_section_code=0&news_office_checked=&related=1&docid=0030012490112&nso=so:r,p:from20240408to20240414,a:all"
+URL_0415_0421 = "https://search.naver.com/search.naver?where=news&sm=tab_tnw&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sort=0&photo=0&field=0&pd=3&ds=2024.04.15&de=2024.04.21&mynews=0&office_type=0&office_section_code=0&news_office_checked=&related=1&docid=0150004975205&nso=so:r,p:from20240415to20240421,a:all"
+URL_0422_0428 = "https://search.naver.com/search.naver?where=news&sm=tab_tnw&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sort=0&photo=0&field=0&pd=3&ds=2024.04.22&de=2024.04.28&mynews=0&office_type=0&office_section_code=0&news_office_checked=&related=1&docid=0150004978196&nso=so:r,p:from20240422to20240428,a:all"
+URL_0429_0505 = "https://search.naver.com/search.naver?where=news&sm=tab_tnw&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sort=0&photo=0&field=0&pd=3&ds=2024.04.29&de=2024.05.05&mynews=0&office_type=0&office_section_code=0&news_office_checked=&related=1&docid=0030012525158&nso=so:r,p:from20240429to20240505,a:all"
+URL_0506_0512 = "https://search.naver.com/search.naver?where=news&sm=tab_tnw&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sort=0&photo=0&field=0&pd=3&ds=2024.05.06&de=2024.05.12&mynews=0&office_type=0&office_section_code=0&news_office_checked=&related=1&docid=1190002829053&nso=so:r,p:from20240506to20240512,a:all"
+URL_0513_0519 = "https://search.naver.com/search.naver?where=news&sm=tab_tnw&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sort=0&photo=0&field=0&pd=3&ds=2024.05.13&de=2024.05.19&mynews=0&office_type=0&office_section_code=0&news_office_checked=&related=1&docid=0310000837374&nso=so:r,p:from20240513to20240519,a:all"
+URL_0520_0526 = "https://search.naver.com/search.naver?where=news&sm=tab_tnw&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sort=0&photo=0&field=0&pd=3&ds=2024.05.20&de=2024.05.26&mynews=0&office_type=0&office_section_code=0&news_office_checked=&related=1&docid=0010014705455&nso=so:r,p:from20240520to20240526,a:all"
 
-# Define the URL
-url = URL_0304_0310
-# url에서 검색구간 날짜 추출
-start_date = url[url.find('ds')+3:url.find('ds')+13]
-end_date = url[url.find('de')+3:url.find('de')+13]
 
-# Setup Selenium WebDriver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get(url)
+urls = [URL_0304_0310, URL_0311_0317, URL_0318_0324, URL_0325_0331,
+        URL_0401_0407, URL_0408_0414, URL_0415_0421, URL_0422_0428,
+        URL_0429_0505, URL_0506_0512, URL_0513_0519, URL_0520_0526]
 
-# Scroll to the bottom of the page to load all news items
-last_height = driver.execute_script("return document.body.scrollHeight")
-while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)  # Wait for the page to load
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
+for url in urls:
+    # url에서 검색구간 날짜 추출
+    start_date = url[url.find('ds')+3:url.find('ds')+13]
+    end_date = url[url.find('de')+3:url.find('de')+13]
 
-# Parse the page content with BeautifulSoup
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-driver.quit()
+    # Setup Selenium WebDriver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(url)
 
-# Extract the relevant information
-news_items = []
-for item in soup.select('li.bx'):
-    title_element = item.select_one('a.news_tit')
-    description_element = item.select_one('div.news_dsc')
-    url_element = item.select_one('a.news_tit')
-    thumbnail_elements = item.select('img')
-    date_elements = item.select('span.info')
-    print(date_elements)
+    # Scroll to the bottom of the page to load all news items
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)  # Wait for the page to load
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
 
-    title = title_element.get_text() if title_element else None
-    description = description_element.get_text() if description_element else None
-    url = url_element['href'] if url_element else None
-    thumbnail = thumbnail_elements[1]['src'] if len(thumbnail_elements)>1 else None
+    # Parse the page content with BeautifulSoup
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.quit()
 
-    if len(date_elements) == 1:
-        date = date_elements[0].get_text()
-    elif len(date_elements) == 2:   # 3면 1단, 2면 TOP 와 같은게 함께 있을 때
-        date_elements[1].get_text()
-    else:
-        date = None
+    # Extract the relevant information
+    news_items = []
+    for item in soup.select('li.bx'):
+        title_element = item.select_one('a.news_tit')
+        description_element = item.select_one('div.news_dsc')
+        url_element = item.select_one('a.news_tit')
+        thumbnail_elements = item.select('img')
+        date_elements = item.select('span.info')
 
-    if title and description and url and date:  # thumnail은 없을 수 있다!
-        # DB column명에 대응되도록 저장
-        news_items.append({
-            'date': date,
-            'title': title,
-            'summary': description,
-            'url': url,
-            'image_url': thumbnail,
-        })
+        title = title_element.get_text() if title_element else None
+        description = description_element.get_text() if description_element else None
+        url = url_element['href'] if url_element else None
+        thumbnail = thumbnail_elements[1]['src'] if len(thumbnail_elements)>1 else None
 
-# Save the data to a JSON file
-with open(f'./data/{start_date}_{end_date}.json', 'w', encoding='utf-8') as f:
-    json.dump(news_items, f, ensure_ascii=False, indent=4)
+        if len(date_elements) == 1:
+            date = date_elements[0].get_text()
+        elif len(date_elements) == 2:   # 3면 1단, 2면 TOP 와 같은게 함께 있을 때
+            date_elements[1].get_text()
+        else:
+            date = None
+
+        if title and description and url and date:  # thumnail은 없을 수 있다!
+            # DB column명에 대응되도록 저장
+            news_items.append({
+                'date': date,
+                'title': title,
+                'summary': description,
+                'url': url,
+                'image_url': thumbnail,
+            })
+
+    # Save the data to a JSON file
+    with open(f'./data/{start_date}_{end_date}.json', 'w', encoding='utf-8') as f:
+        json.dump(news_items, f, ensure_ascii=False, indent=4)
